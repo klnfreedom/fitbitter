727dff7060ceadb847d232647ce2276d1c3c7120
klnfreedom <klnfreedom@gmail.com>
2025-07-02 12:23:11 +0300

    feat: refactor heart rate data model and manager for improved data handling and extraction

--- lib/src/managers/fitbitHeartRateIntradayMananger.dart
+++ lib/src/managers/fitbitHeartRateIntradayMananger.dart
@@ -1,7 +1,7 @@
-import 'dart:convert';
-
 import 'package:fitbitter/fitbitter.dart';
 import 'package:logger/logger.dart';
+
+import '../utils/formats.dart';
 
 class FitbitIntradayHeartRateManager extends FitbitDataManager {
   FitbitIntradayHeartRateManager({
@@ -11,7 +11,8 @@ class FitbitIntradayHeartRateManager extends FitbitDataManager {
     required super.onResetCredentials,
   });
 
-  Future<FitbitData> fetchData(FitbitAPIURL fitbitUrl) async {
+  @override
+  Future<List<FitbitData>> fetch(FitbitAPIURL fitbitUrl) async {
     // Get the response
     final response = await getResponse(fitbitUrl: fitbitUrl);
 
@@ -19,10 +20,33 @@ class FitbitIntradayHeartRateManager extends FitbitDataManager {
     final logger = Logger();
     logger.i('$response');
 
-    final data = response.data is String ? jsonDecode(response.data) : response.data;
-    return FitbitIntradayHeartRate.fromJson(data['activities-heart-intraday'] ?? {});
-  }
+    //Extract data and return them
+    List<FitbitData> ret = _extractFitbitHeartRateIntradayData(
+        response, fitbitUrl.fitbitCredentials!.userID);
+    return ret;
+  } // fetch
 
-  @override
-  Future<List<FitbitData>> fetch(FitbitAPIURL url) => throw UnimplementedError();
+  /// A private method that extracts [FitbitHeartData] from the given response.
+  List<FitbitHeartRateIntradayData> _extractFitbitHeartRateIntradayData(
+      dynamic response, String? userId) {
+    final dateOfMonitoring =
+    DateTime.parse(response['activities-heart'][0]['dateTime']);
+    final data = response['activities-heart-intraday']['dataset'];
+    List<FitbitHeartRateIntradayData> heartDataPoints =
+    List<FitbitHeartRateIntradayData>.empty(growable: true);
+
+    if (data.isNotEmpty) {
+      for (var record in data) {
+        final time = Formats.onlyTimeWithSeconds.parse(record['time']);
+        heartDataPoints.add(FitbitHeartRateIntradayData(
+          userID: userId,
+          dateOfMonitoring: dateOfMonitoring.add(Duration(
+              hours: time.hour, minutes: time.minute, seconds: time.second)),
+          value: record['value'].toDouble(),
+        ));
+      } // for entry
+    }
+
+    return heartDataPoints;
+  }
 }
